255576671db162683e03b26b2575078b2c9bf242
tkkb1 <mafukase@icloud.com>
2023-10-19 16:03:37 -0300

    criacao servidor/banco de dados

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const pool = require('./conexao');
+const bodyParser = require('body-parser');
 
-const app = express;
+const app = express();
 
-app.get('/', (req, res) => {
-    
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.post('/', async (req, res) => {
+    const { nome, telefone, email } = req.body;
+    try {
+        const resultado = await pool.query('INSERT INTO registros (nome, telefone, email) VALUES ($1, $2, $3)', [nome, telefone, email]);
+        res.json(resultado);
+    } catch (error) {
+        console.log(error.message);
+    }
 });
 
-app.listen(5501);
+app.listen(5502);
