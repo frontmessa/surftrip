a9b652f0fa9c854d04a44b58e42b8292fa243140
tkkb1 <mafukase@icloud.com>
2023-10-23 19:36:08 -0300

    tabela.ejs

--- controller.js
+++ controller.js
@@ -3,7 +3,8 @@ const pool = require('./db.js');
 const getRegistros = (req, res) => {
     pool.query("SELECT nome, telefone, email FROM registros", (error, results) => {
         if (error) throw error;
-        res.render('/registros', { registros: results.rows });
+        console.log(results.rows);
+        res.render('tabela', {data:results.rows});
       });
  };
 
