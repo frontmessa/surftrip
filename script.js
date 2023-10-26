98be0c7732401f327e6f23f3c4de35d27212f1be
frontmessa <caio-messa@hotmail.com>
2023-10-26 00:34:34 -0300

    tabelajs

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 "use strict";
 
+
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+  modal.classList.toggle("hide");
+  fade.classList.toggle("hide");
+};
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+  el.addEventListener("click", () => toggleModal());
+});
+
+
 /**
  * navbar toggle
  */
@@ -78,3 +94,4 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
+
