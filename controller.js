d4aa818e7a0b179b7755da5d1d16713e80448ef6
frontmessa <caio-messa@hotmail.com>
2023-10-30 17:17:50 -0300

    idade dash

--- controller.js
+++ controller.js
@@ -9,7 +9,7 @@ const { error } = require("console");
 
 const getRegistros = (req, res) => {
   pool.query(
-    "SELECT nome, telefone, email FROM registros",
+    "SELECT nome, telefone, email, idade FROM registros",
     (error, results) => {
       if (error) throw error;
       res.render("dash", { data: results.rows });
@@ -18,37 +18,37 @@ const getRegistros = (req, res) => {
 };
 
 const login = async (req, res) => {
-  const { email, senha } = req.body;
+  console.log(req)
 
-  try {
-    const usuario = await pool.query("select * from login where email = $1", [
-      email,
-    ]);
-    res.sendFile(path.join(__dirname, "public/login.html"));
+//   try {
+//     const usuario = await pool.query("select * from login where email = $1", [
+//       email,
+//     ]);
+//     res.sendFile(path.join(__dirname, "public/login.html"));
 
-    if (usuario.rowCount < 1) {
-      return res.send({ mensagem: "Email ou senha invalida" });
-    }
+//     if (usuario.rowCount < 1) {
+//       return res.send({ mensagem: "Email ou senha invalida" });
+//     }
 
-    const senhaValida = await bcrypt.compare(senha, usuario.rows[0].senha);
+//     const senhaValida = await bcrypt.compare(senha, usuario.rows[0].senha);
 
-    if (!senhaValida) {
-      return res.send({ mensagem: "Email ou senha invalida" });
-    }
+//     if (!senhaValida) {
+//       return res.send({ mensagem: "Email ou senha invalida" });
+//     }
 
-    const token = jwt.sign({ id: usuario.rows[0].id }, senhaJwt, {
-      expiresIn: "8h",
-    });
+//     const token = jwt.sign({ id: usuario.rows[0].id }, senhaJwt, {
+//       expiresIn: "8h",
+//     });
 
-    const { senha: _, ...usuarioLogado } = usuario.rows[0];
+//     const { senha: _, ...usuarioLogado } = usuario.rows[0];
 
-    return res.json({ usuario: usuarioLogado, token });
+//     return res.json({ usuario: usuarioLogado, token });
 
-    //req.usuarioId = decoded.id;
-    next();
-  } catch (error) {
-    return res.send(error.message);
-  }
+    
+//     next();
+//   } catch (error) {
+//     return res.send(error.message);
+//   }
 };
 
 const criandoAcesso = async (req, res) => {
@@ -67,12 +67,12 @@ const criandoAcesso = async (req, res) => {
   }
 };
 const cadastroCliente = async (req, res) => {
-  const { nome, telefone, email } = req.body;
+  const { nome, telefone, email, idade} = req.body;
 
   try {
     const resultado = await pool.query(
-      "INSERT INTO registros (nome, telefone, email) VALUES ($1, $2, $3)",
-      [nome, telefone, email]
+      "INSERT INTO registros (nome, telefone, email, idade) VALUES ($1, $2, $3, $4)",
+      [nome, telefone, email, idade]
     );
     res.sendFile(path.join(__dirname, "public/index.html"));
   } catch (error) {
--- public/js/script.js
+++ public/js/script.js
@@ -9,7 +9,7 @@ charts.forEach(function (chart) {
       datasets: [
         {
           label: "# of Votes",
-          data: [12, 19, 3, 5, 2, 3],
+          data: [12, 27, 3, 5, 2, 3],
           backgroundColor: [
             "rgba(255, 99, 132, 0.2)",
             "rgba(54, 162, 235, 0.2)",
